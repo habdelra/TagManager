93e3e7ac05edaa0db26905a5d38ee9b260a00d53
Hassan Abdel-Rahman <hassan.abdelrahman@mhelabs.com>
2014-08-25 19:39:53 -0400

    made edit button a toggle

--- app/components/tag-item.js
+++ app/components/tag-item.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 
 var get = Ember.get;
+var set = Ember.set;
 
 export default Ember.Component.extend({
-  clasNames: ['tag'],
+  classNameBindings: [":tag", "isOn:selected"],
 
   actions: {
     editTag: function() {
+      var isOn = get(this, 'isOn');
+      set(this, 'isOn', !isOn);
       console.log('edit tag action clicked for ' + get(this, 'tag.name'));
     }
   }
